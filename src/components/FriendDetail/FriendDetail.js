7b310fdb7fff0cb4ba13c964584db02b4e8d3ff8
Your Name <you@example.com>
2021-05-04 10:30:18 -0800

    Hello

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { useEffect, useState } from 'react/cjs/react.development';
+
 
 const FriendDetail = () => {
     const {friendId} = useParams();
